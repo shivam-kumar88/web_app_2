dcc8966e2317bc248d06e02be15793ab5bf633da
shivam <shivam882531@gmail.com>
2021-07-11 01:17:48 +0530

    ui of save page updated

--- components/main/save.js
+++ components/main/save.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-import {View, TextInput, Button, Image } from 'react-native'
+import {View, TextInput, Button, Image, StyleSheet, ImageBackground, TouchableOpacity, Text } from 'react-native'
 import firebase from 'firebase'
 require("firebase/firestore")
 require("firebase/firebase-storage")
+
+const img = {uri : "https://images.unsplash.com/photo-1524408504872-4d40d453c67f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"};
 
 export default function save(props, { navigation }) {
     console.log(props.route.params.image)
@@ -60,15 +62,49 @@ export default function save(props, { navigation }) {
 
 
     return (
-        <View style = {{flex:1}}>
+        <ImageBackground style={styles.image} source={img}>
+        <View style = {styles.container}>
             <Image source = {{uri:props.route.params.image}}/>
-            <TextInput
-            placeholder="Write any caption."
-            onChangeText={(caption)=>setCaption(caption)}/>
-            <Button
-            title="upload image"
-            onPress = {() => uploadImage()}/>
+            
+            <TextInput style={styles.input} placeholder="Enter your message here" onChangeText={(caption)=>setCaption(caption)}/>
+            <TouchableOpacity style = {styles.buttonContainer1} onPress = {() => uploadImage()}>
+                <Text>upload</Text>
+            </TouchableOpacity>
 
         </View>
+        </ImageBackground>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 30,
+    },
+    buttonContainer1: {
+        marginTop:10,
+        height:45,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom:20,
+        width:250,
+        borderRadius:30,
+        backgroundColor: "#50e38f",
+    },
+    input: {
+        height: 50,
+        width: '100%',
+        borderWidth: 1,
+        padding: 15,
+        marginBottom: 20,
+        borderColor: 'white',
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+    },
+})
